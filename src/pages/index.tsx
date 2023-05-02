8072f82108b6dda865adc8abf80cc707ee7923ba
Arman Torosyan <arman.torosyan@tirsoft.co>
2023-05-02 15:08:26 +0400

    Add Text Copy logic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,13 +4,27 @@ import styles from "./page.module.css";
 import { Button } from "@components/Common/Common";
 import FormDrawer from "@components/Common/Drawer/Drawer";
 import CheckboxButtons from "@components/Common/CheckboxButtons/CheckboxButtons";
+import { useState } from "react";
 
 export default function Home() {
+  const [textToCopy, setTextToCopy] = useState("This is the text to copy testststs");
+  const [copied, setCopied] = useState(false);
+
+  const copyTextToClipboard = (text: string) => {
+    navigator.clipboard.writeText(text);
+    setCopied(true);
+  };
 
   return (
     <div>
       Hello World
-      <FormDrawer  />
+      <FormDrawer />
+      <div style={{ paddingTop: 30, display: "flex", flexDirection: "column" }}>
+        <Button rounded onClick={() => copyTextToClipboard(textToCopy)}>
+          {copied ? "Text Copied" : "Copy Text"}
+        </Button>
+        <span style={{ paddingTop: 30 }}>{textToCopy}</span>
+      </div>
     </div>
   );
 }
